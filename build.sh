afbbb3cbfc101d5316c4d17bc017b4e047824f8d
Andrea Maccis <andrea.maccis@gmail.com>
2025-05-08 21:34:51 +0200

    Update build and dockerfile for dynamic building

--- build.sh
+++ build.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-IMAGE_VERSION="8.4.2-2.2.0"
-IMAGE_TAG=amaccis/php-libstemmer:$IMAGE_VERSION
+if [ -z "$1" ]; then
+  echo "Error: missing first argument (PHP_VERSION)."
+  exit 1
+fi
 
-docker build -t $IMAGE_TAG .
+if [ -z "$2" ]; then
+  echo "Error: missing second argument (ALPINE_VERSION)."
+  exit 1
+fi
+
+if [ -z "$3" ]; then
+  echo "Error: missing third argument (LIBSTEMMER_VERSION)."
+  exit 1
+fi
+
+PHP_VERSION="$1"
+ALPINE_VERSION="$2"
+LIBSTEMMER_VERSION="$3"
+
+IMAGE_VERSION="$PHP_VERSION-$LIBSTEMMER_VERSION"
+IMAGE_TAG="amaccis/php-libstemmer:$IMAGE_VERSION"
+
+docker build -t $IMAGE_TAG \
+  --build-arg php_version=$PHP_VERSION \
+  --build-arg alpine_version=$ALPINE_VERSION \
+  --build-arg libstemmer_version=$LIBSTEMMER_VERSION .
